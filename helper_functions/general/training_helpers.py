114c74f0344c7a1fbc4c9164745ab73aca0df761
cameronjm123 <cameronmills182@gmail.com>
2024-11-03 12:38:30 +0000

    fixes

--- helper_functions/general/training_helpers.py
+++ helper_functions/general/training_helpers.py
@@ -6,9 +6,11 @@ from sklearn.pipeline import Pipeline
 
 
 class TrainPipelineParallel:
-    def __init__(self, pre_grouped_pipeline: Pipeline, post_grouped_pipeline_func: Callable[[], Pipeline]):
+    def __init__(self, pre_grouped_pipeline: Pipeline, post_grouped_pipeline_func: Callable[[], Pipeline], grouping: str, other_index_columns: list[str] = None):
         self.pre_grouped_pipeline = pre_grouped_pipeline
         self.post_grouped_pipeline_func = post_grouped_pipeline_func
+        self.grouping = grouping
+        self.index_columns = [grouping] + other_index_columns
         self.models = {}
 
     def train_pipeline_for_column(self, group_label: str, grouped_train_data: pd.DataFrame) -> (str, Pipeline):
@@ -20,22 +22,24 @@ class TrainPipelineParallel:
 
         return group_label, pipeline
 
-    def predict_for_column(self, group_label: str, grouped_test_data: pd.DataFrame) -> (str, pd.DataFrame):
+    def predict_for_column(self, group_label: int | str, grouped_test_data: pd.DataFrame) -> (str, pd.DataFrame):
         trained_pipeline = self.models[group_label]
-        return trained_pipeline.predict(grouped_test_data)
+        return pd.DataFrame(trained_pipeline.predict(grouped_test_data), index=grouped_test_data.index)
 
-    def train_parallel(self, X_train: pd.DataFrame, y_train: pd.DataFrame, grouping: str, n_jobs=-1):
+    def train_parallel(self, X_train: pd.DataFrame, y_train: pd.DataFrame, n_jobs=-1):
         self.pre_grouped_pipeline.fit(X_train)
         X_train_transformed = self.pre_grouped_pipeline.transform(X_train)
 
+        print('Completed pre-train transformations')
         data_train = pd.merge(X_train_transformed, y_train)
+        data_train.set_index(self.index_columns, inplace=True)
         models = Parallel(n_jobs=n_jobs)(delayed(self.train_pipeline_for_column)(group_label, grouped_data)
-                                         for group_label, grouped_data in data_train.groupby(grouping))
+                                         for group_label, grouped_data in data_train.groupby(self.grouping))
         self.models = dict(models)
 
-    def predict_parallel(self, X_test: pd.DataFrame, grouping: str, n_jobs=-1):
+    def predict_parallel(self, X_test: pd.DataFrame, n_jobs=-1):
         X_test_transformed = self.pre_grouped_pipeline.transform(X_test)
-        predictions = Parallel(n_jobs=n_jobs)(delayed(self.train_pipeline_for_column)(group_label, grouped_data)
-                                         for group_label, grouped_data in X_test_transformed.groupby(grouping))
-        predictions_dict = dict(predictions)
-        return pd.concat(predictions_dict.values(), keys=predictions_dict.keys(), names=[grouping])
+        X_test_transformed.set_index(self.index_columns, inplace=True)
+        predictions = Parallel(n_jobs=n_jobs)(delayed(self.predict_for_column)(group_label, grouped_data)
+                                         for group_label, grouped_data in X_test_transformed.groupby(self.grouping))
+        return pd.concat(predictions).rename(columns={0: 'target'})
